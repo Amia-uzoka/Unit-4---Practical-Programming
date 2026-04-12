74c262d94e5f380a8638166705d0e81f2d8ba03c
Amia-uzoka <7056@winchmore.enfield.sch.uk>
2026-04-12 08:05:48 +0100

    Update unit 4 practical programming.py
    Committed first draft of code for feature 1 (reading data into a text file)

---
+++ unit 4 practical programming.py
@@ -0,0 +1,26 @@
+def read_data(filename): #read vendor data from text file and store them
+    
+    data = [] # dataset
+
+    with open(filename, "r") as file: #with statement makes sure file is closed after use
+        lines = file.readlines() #The readlines() method returns a list containing each line in the file as a list item.
+
+        for item in lines:
+            columns = item.strip().split(",")
+            
+        
+            row = [
+                
+                columns[0], #vendor ID
+                columns[1], #Vendor name 
+                columns[2], # year and week 
+                int(columns[3]), #number of vegan hotdogs 
+                int(columns[4]), # Number of meat hotdogs 
+                float(columns[5]), # Onions(Kg)
+                float(columns[6]), # Ketchup (litres)
+                
+
+                ]
+            data.append(row)
+        return data
+print(read_data("Hotdogs (1).txt"))
