0937a743bd6a30ad40205d1189f786cedd385648
Amia-uzoka <7056@winchmore.enfield.sch.uk>
2026-04-22 10:46:20 +0100

    Update unit 4 practical programming.py

--- unit 4 practical programming.py
+++ unit 4 practical programming.py
@@ -503,3 +503,210 @@ if user_input_view_sorted_file.lower()== "yes": #allow user option to see sorted
         print(row)
 else:
     exit
+    #--------------------------------------------------------------------------------------------------------------------------------------------
+    from datetime import date
+import sys
+
+
+def read_data(filename): #read vendor data from text file and store them
+    
+    data = [] # dataset
+    
+    try:
+
+        with open(filename, "r") as file: #with statement makes sure file is closed after use
+            lines = file.readlines() #The readlines() method returns a list containing each line in the file as a list item.
+
+    except FileNotFoundError:
+         print(" Error: File not found")
+
+        
+    for item in lines:
+            columns = item.strip().split(",")
+            
+        
+            row = [
+                
+                columns[0], #vendor ID
+                columns[1], #Vendor name 
+                columns[2], # year and week 
+                int(columns[3]), #number of vegan hotdogs 
+                int(columns[4]), # Number of meat hotdogs 
+                float(columns[5]), # Onions(Kg)
+                float(columns[6]), # Ketchup (litres)
+                
+
+                     ]
+            data.append(row)
+
+            
+    # Unit test 1
+    # Test 1
+    #for row in data:
+        #print(row)
+    # Test 2
+    #print(data[9][0])
+    # Test3
+    
+    
+    
+        
+    return data
+    
+
+hotdog_data = read_data("Hotdogs (1).txt")
+#-----------------------------------------------------------------------------------------------------------------------------
+def linear_search_unsorted(data):
+    target = input("Search Vendors")
+    
+    for row in hotdog_data:
+        if row[1].lower() == target.lower():
+         
+           
+            print("Vendor ID:", row[0])
+            print("Vendor name:", row[1])
+            print("Year and Week:", row[2])
+            print("Number of vegan hotdogs:", row[3])
+            print("Number of meat hotdogs:", row[4])
+            print("Onions (Kg):", row[5])
+            print("Ketchup (Litres):", row[6])
+            return       
+    else:
+        print("vendor not found")
+
+               
+#linear_search_unsorted(hotdog_data)
+
+
+#linear search nyw (name year week)
+def linear_search_unsorted_nyw(data):
+    target_name = input("Search Vendors")
+    target_yw = int(input("enter year week in format yyyywwww"))
+    
+    for row in hotdog_data:
+        if row[1].lower() == target_name.lower() and int(row[2])== target_yw:
+         
+            
+            print("Vendor ID:", row[0])
+            print("Vendor name:", row[1])
+            print("Year and Week:", row[2])
+            print("Number of vegan hotdogs:", row[3])
+            print("Number of meat hotdogs:", row[4])
+            print("Onions (Kg):", row[5])
+            print("Ketchup (Litres):", row[6])
+            return
+        
+
+    print("vendor not found")
+
+
+               
+#linear_search_unsorted_nyw(hotdog_data)
+
+
+
+def search_menu(data):
+    print( "Option 1 search by vendor name")
+    print("option 2 search by vendor name, year and week")
+
+    choice = int(input("enter choice 1 or 2"))
+
+    if choice == 1 :
+        linear_search_unsorted(hotdog_data)
+
+    elif choice == 2 :
+        linear_search_unsorted_nyw(hotdog_data)
+    else:
+        print("invalid option")
+#-----------------------------------------        
+
+search_menu(hotdog_data)
+#----------------------------------------
+
+
+
+        
+
+def add_vendor(data):
+    
+    vendor_id = input("Enter vendor id in the format - XX_DDD - ").upper()
+    
+    if len(vendor_id) <6 or len(vendor_id) > 6:
+        print("invalid year and week")
+
+        
+    f_alpha_vendor_id = vendor_id [0:2]
+    f_numb_vendor_id = vendor_id [4:6]
+    f_u_vendor_id = vendor_id [3]
+     
+    
+    if int(f_numb_vendor_id).isdigit() == False or f_alpha_vendor_id.isalpha == False or f_u_vendor_id != "_" :
+        print("invalid vendor id given")
+
+    
+    
+#-----------------------------------------------------------------------------
+    #validation
+    name_valid = True
+ 
+    try:
+        vendor_name = input("Enter vendor name in format")
+ 
+        if not vendor_name or not all(c.isalpha() or c.isspace() for c in vendor_name):
+            raise ValueError
+        if len(vendor_name) < 2 or len(vendor_name) > 25:
+            print("Invalid vendor name")
+            name_valid = False
+    except ValueError:
+        print("invalid vendor name")
+        name_valid = False
+        
+            
+        
+# -----------------------------------------------------------------------------------------
+
+    year_week = input("Year and week of info in format [ YYYYWW ] ")
+    if len(year_week) < 7  or  len(vendor_id) > 7:
+        print("invalid year and week")
+        
+    #format_check_yw = year_week.split("-")
+    current_year = date.today().year()
+    
+    formatweek = year_week [4:6]
+    formatyear = year_week [0:3]
+    
+    if int(formatweek) < 1 or int(formatweek) > 52 and int(formatyear) > current_year:
+        print("invalid year and week given please enter in the correct format")
+  
+    
+#-------------------------------------------------------------------------------------------
+
+   number_of_vegan = input("Enter number of vegan hotdogs in format ")
+    
+                  number_of_meat_hotdogs = input("Enter number of meatdogs in format ")
+    
+    onions = float(input("Enter Kg of onions "))
+    
+    ketchup = float(input("Enter ketchup in litres "))
+    
+    new_vendor = [
+        
+        vendor_id,
+        vendor_name,
+        number_of_vegan,
+        number_of_meat_hotdogs,
+        onions,
+        ketchup
+        
+        ]
+    print(new_vendor)
+    user 
+    corret_vendor_data =
+    safe_data = data.copy
+    safe_data.append(new_vendor)
+    print("Vendor added")
+  
+
+add_vendor(hotdog_data)
+
+print(hotdog_data)
